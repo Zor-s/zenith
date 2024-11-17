38d28882323914a20be099e22a62a437f3dcc33f
DrZors <drzoren@gmail.com>
2024-11-17 15:56:08 +0800

    added redirect

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=./public/index.php">
+<title>Redirecting...</title>
+</head>
+<body>
+  If you're not redirected, <a href="./public/index.php">click here</a>. 
+</body>
+</html>
